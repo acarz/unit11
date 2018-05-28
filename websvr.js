ce6d2dc2d158d76dea2ef4194f5961b4d5cde75c
acarz <zaferacar@gmail.com>
2018-05-27 23:28:59 -0700

    Update websvr.js

--- websvr.js
+++ websvr.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write('Hello World,my name is Zafer Acar'); 
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.write('Hello World, my name is Zafer Acar'); 
 	response.end(); 
 }).listen(8001); 
